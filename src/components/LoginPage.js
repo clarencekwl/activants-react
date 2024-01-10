01e6d2763c77d1557e7dabb5e12cf4b02cd9be03
ClarenceKWL <clarencekway@gmail.com>
2024-01-10 15:26:07 +0800

    :recycle: [Refactor] added css files

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,53 +1,53 @@
-// LoginPage.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
 import { useFirestore } from '../FirestoreContext';
+import '../styles/AuthStyle.css';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [submitting, setSubmitting] = useState(false)
   const navigate = useNavigate();
   const { login } = useAuth();
   const { initUser } = useFirestore();
 
   const handleLogin = async () => {
-    // Validation: Check if email and password are not empty
     if (!email.trim() || !password.trim()) {
       setError('Email and password are required.');
       return;
     }
 
     try {
-      // Sign in the user using Firebase authentication
+      setSubmitting(true);
       const user = await login(email, password);
       console.log("user id after login: ", user.uid);
       await initUser(user.uid);
-      // Clear previous error message
       setError('');
 
-      // Redirect to the posts page
       navigate('/posts', { replace: true });
 
     } catch (error) {
-      // Handle authentication errors
       setError('Authentication failed. Please check your email and password.');
       console.error('Error signing in:', error);
+    } finally {
+      setSubmitting(false);
     }
   };
 
+
   return (
-    <div style={containerStyle}>
+    <div className="container">
       <h2 style={{ color: '#333' }}>Login Page</h2>
-      <form style={formStyle}>
+      <form className="form">
         <label>
           Email:
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            style={inputStyle}
+            className="input"
           />
         </label>
         <br />
@@ -57,50 +57,16 @@ const LoginPage = () => {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            style={inputStyle}
+            className="input"
           />
         </label>
         <br />
         {error && <p style={{ color: 'red' }}>{error}</p>}
-        <button type="button" onClick={handleLogin} style={buttonStyle}>
+        <button type="button" disabled={submitting} onClick={handleLogin} className="button">
           Login
         </button>
       </form>
     </div>
   );
 };
-
-
-const containerStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center', // Center horizontally
-  justifyContent: 'center', // Center vertically
-  height: '100vh', // Use full viewport height
-  marginTop: '-50px', // Adjust the top margin to move the form higher up
-};
-
-const formStyle = {
-  background: '#f0f0f0', // Set background color
-  padding: '20px',
-  borderRadius: '8px',
-  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', // Add a subtle box shadow
-};
-
-const inputStyle = {
-  marginBottom: '10px',
-  padding: '8px',
-  width: '100%',
-  boxSizing: 'border-box',
-};
-
-const buttonStyle = {
-  background: '#333', // Set background color
-  color: '#fff', // Set text color
-  padding: '10px',
-  borderRadius: '5px',
-  cursor: 'pointer',
-  width: '100%', // Make the button full width
-};
-
 export default LoginPage;
--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,26 +1,26 @@
-// SignUpPage.js
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
 import { useFirestore } from '../FirestoreContext';
+import '../styles/AuthStyle.css';
 
 const SignUpPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
   const [error, setError] = useState('');
+  const [submitting, setSubmitting] = useState(false)
   const navigate = useNavigate();
   const { signup } = useAuth();
   const { addUserToFirestore } = useFirestore();
 
   const handleSignUp = async () => {
-    // Validation: Check if email and password are not empty
     if (!email.trim() || !password.trim()) {
       setError('Email and password are required.');
       return;
     }
 
-    // Password validation
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}|:"<>?~`]).{8,}$/;
     if (!passwordRegex.test(password)) {
       setError('Password must contain at least one lowercase letter, one uppercase letter, and one special character.');
@@ -28,45 +28,43 @@ const SignUpPage = () => {
     }
 
     try {
-      // Sign up the user using the signup function from useAuth
+      setSubmitting(true);
       const user = await signup(email, password);
 
-      // Add user information to Firestore
       await addUserToFirestore(user.uid, email, username);
 
-      // Clear previous error message
       setError('');
 
-      // Redirect to the login page
       navigate('/posts', { replace: true });
 
     } catch (error) {
-      // Handle authentication errors
       setError('Sign-up failed. Please check your email and password.');
       console.error('Error signing up:', error);
+    } finally {
+      setSubmitting(false);
     }
   };
   return (
-    <div style={containerStyle}>
+    <div className="container">
       <h2 style={{ color: '#333' }}>Sign Up</h2>
-      <form style={formStyle}>
+      <form className="form">
         <label>
           Email:
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            style={inputStyle}
+            className="input"
           />
         </label>
         <br />
         <label>
-          Username: {/* Add a username input field */}
+          Username:
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            style={inputStyle}
+            className="input"
           />
         </label>
         <br />
@@ -76,50 +74,17 @@ const SignUpPage = () => {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            style={inputStyle}
+            className="input"
           />
         </label>
-
         <br />
         {error && <p style={{ color: 'red' }}>{error}</p>}
-        <button type="button" onClick={handleSignUp} style={buttonStyle}>
+        <button type="button" disabled={submitting} onClick={handleSignUp} className="button">
           Sign Up
         </button>
       </form>
     </div>
   );
-};
-
-const containerStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center', // Center horizontally
-  justifyContent: 'center', // Center vertically
-  height: '100vh', // Use full viewport height
-  marginTop: '-50px', // Adjust the top margin to move the form higher up
-};
-
-const formStyle = {
-  background: '#f0f0f0', // Set background color
-  padding: '20px',
-  borderRadius: '8px',
-  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', // Add a subtle box shadow
-};
-
-const inputStyle = {
-  marginBottom: '10px',
-  padding: '8px',
-  width: '100%',
-  boxSizing: 'border-box',
-};
-
-const buttonStyle = {
-  background: '#333', // Set background color
-  color: '#fff', // Set text color
-  padding: '10px',
-  borderRadius: '5px',
-  cursor: 'pointer',
-  width: '100%', // Make the button full width
 };
 
 export default SignUpPage;
